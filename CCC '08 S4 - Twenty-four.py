ace850e8871913c20925118fe37cbd41c8ae64fc
DeadUser123 <108906627+DeadUser123@users.noreply.github.com>
2024-06-29 11:38:22 -0400

    Update CCC '08 S4 - Twenty-four.py
    Renamed variables to be more descriptive

--- CCC '08 S4 - Twenty-four.py
+++ CCC '08 S4 - Twenty-four.py
@@ -1,50 +1,54 @@
-ans = 0.14
+ans = None
 
-def start(li):
-    for i in li:
-        x = li[:]
-        x.remove(i)
-        twentyfour(i, x)
+def begin_solving(nums):
+    for num in nums:
+        nums_left = nums[:]
+        nums_left.remove(num)
+        twentyfour(num, nums_left)
         
-def twentyfour(current, li):
+def twentyfour(current_result, nums_left):
     global ans
-    if len(li) == 0:
-        if ans == 0.14 and current <= 24:
-            ans = current
-        elif current <= 24 and current > ans:
-            ans = current
-        return current
-    for i in li:
-        x = li[:]
-        x.remove(i)
-        twentyfour(current + i, x)
-        twentyfour(current - i, x)
-        twentyfour(i - current, x)
-        twentyfour(current * i, x)
-        if i != 0 and current % i == 0:
-            twentyfour(current // i, x)
-        if current != 0 and i % current == 0:
-            twentyfour(i // current, x)
-    if len(li) == 2:
-        twentyfour(current, [li[0] + li[1]])
-        twentyfour(current, [li[0] - li[1]])
-        twentyfour(current, [li[1] - li[0]])
-    
+    if len(nums_left) == 0:
+        if ans == None and current_result <= 24:
+            ans = current_result
+        elif current_result <= 24 and current_result > ans:
+            ans = current_result
+        return
+    for num in nums_left:
+        updated_nums_left = nums_left[:]
+        updated_nums_left.remove(num)
+        twentyfour(current_result + num, updated_nums_left)
+        twentyfour(current_result - num, updated_nums_left)
+        twentyfour(num - current_result, updated_nums_left)
+        twentyfour(current_result * num, updated_nums_left)
+        if num != 0 and current_result % num == 0:
+            twentyfour(current_result // num, updated_nums_left)
+        if current_result != 0 and num % current_result == 0:
+            twentyfour(num // current_result, updated_nums_left)
+    if len(nums_left) == 2:
+        twentyfour(current_result, [nums_left[0] + nums_left[1]])
+        twentyfour(current_result, [nums_left[0] - nums_left[1]])
+        twentyfour(current_result, [nums_left[1] - nums_left[0]])
+        twentyfour(current_result, [nums_left[0] * nums_left[1]])
+        if nums_left[0] % nums_left[1] == 0 and nums_left[1] != 0:
+            twentyfour(current_result, [nums_left[0] // nums_left[1]])
+        if nums_left[1] % nums_left[0] == 0 and nums_left[0] != 0:
+            twentyfour(current_result, [nums_left[1] // nums_left[0]])
 
 n = int(input())
-done = dict()
+computed = dict()
 for _ in range(n):
-    ans = 0.14
-    res = []
+    ans = None
+    inputs = []
     for _ in range(4):
-        res.append(int(input()))
-    res.sort()
+        inputs.append(int(input()))
+    inputs.sort()
     
     # if we've already computed the answer for this set of numbers
-    if tuple(res) in done.keys():
-        print(done[tuple(res)])
+    if tuple(inputs) in computed.keys():
+        print(computed[tuple(inputs)])
         continue
     
-    start(res)
+    begin_solving(inputs)
     print(ans)
-    done[tuple(res)] = ans
+    computed[tuple(inputs)] = ans
