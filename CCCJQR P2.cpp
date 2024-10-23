4b0e62c0a1ad536a110bd469248cad7b5cc5d015
DeadUser123 <108906627+DeadUser123@users.noreply.github.com>
2024-10-23 10:30:48 -0400

    Create CCCJQR P2.cpp

---
+++ CCCJQR P2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string line;
+    getline(cin, line);
+    int n;
+    cin >> n;
+    cout << n; // John Cena ALWAYS wins
+    return 0;
+}
